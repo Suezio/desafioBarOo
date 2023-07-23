42b5e501eb83f6a1ced8ef3f1fae5d0a0df8cc14
User <sueziooliveira@gmail.com>
2023-07-23 11:19:35 -0300

    gitingore .iml

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+package application;
+import entities.Bill;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Program {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Sexo: ");
+        char gender = scanner.next().charAt(0);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Quantidade de cervejas: ");
+        int beer = scanner.nextInt();
+
+        System.out.print("Quantidade de refrigerantes: ");
+        int softDrink = scanner.nextInt();
+
+        System.out.print("Quantidade de espetinhos: ");
+        int barbecue = scanner.nextInt();
+
+
+        scanner.close();
+
+        Bill bill = new Bill(gender, beer, barbecue, softDrink);
+        System.out.println(bill.toString());
     }
 }
--- src/entities/Bill.java
+++ src/entities/Bill.java
@@ -1,2 +1,66 @@
-package entities;public class Bill {
+package entities;
+
+import java.text.DecimalFormat;
+
+public class Bill {
+    public char gender;
+    public int beer;
+    public int barbecue;
+    public int softDrink;
+    private double beerPrice = 5.00;
+    private double barbecuePrice = 7.00;
+    private double softDrinkPrice = 3.00;
+    private double maleTicketPrice = 10.00;
+    private double femaleTicketPrice = 8.00;
+    private double coverCharge = 4.00;
+
+    public Bill(char gender, int beer, int barbecue, int softDrink) {
+        this.gender = gender;
+        this.beer = beer;
+        this.barbecue = barbecue;
+        this.softDrink = softDrink;
+    }
+
+    private double calculateConsumption() {
+        double totalBeer = beer * beerPrice;
+        double totalBarbecue = barbecue * barbecuePrice;
+        double totalSoftDrink = softDrink * softDrinkPrice;
+
+        return totalBeer + totalBarbecue + totalSoftDrink;
+    }
+
+    private double cover() {
+        double consumption = calculateConsumption();
+        return consumption > 30.00 ? 0.00 : coverCharge;
+    }
+
+    private double feeding() {
+        return calculateConsumption() + cover();
+    }
+
+    private double ticket() {
+        return gender == 'F' ? femaleTicketPrice : maleTicketPrice;
+    }
+
+    public double total() {
+        return feeding() + ticket();
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nRELATÓRIO:");
+        sb.append("\nConsumo = R$ ").append(df.format(calculateConsumption())).append("\n");
+        if (cover() > 0) {
+            sb.append("Couvert = R$ ").append(df.format(cover())).append("\n");
+        } else {
+            sb.append("Isento de Couvert\n");
+        }
+        sb.append("Ingresso = R$ ").append(df.format(ticket())).append("\n");
+        sb.append("Valor a pagar = R$ ").append(df.format(total())).append("\n");
+
+        return sb.toString();
+    }
 }
